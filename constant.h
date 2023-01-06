4eb243c3a6a64070a7af558f4c261bf7daca532d
Maxime <2maxime0>
2023-01-06 12:16:18 +0100

    Added definition of Constant

---
+++ constant.h
@@ -0,0 +1,12 @@
+//Constant class definition with an integer value and the necessary methods to create it and manipulate it
+
+class Constant :
+{
+    private:
+        int value; //Value in private to keep it constant
+
+    public:
+        Constant(int value);
+        int getValue();
+};
+
