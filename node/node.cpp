6f437022b6ea3e04f81c66bdde073446c53853ed
kevin <death-joke>
2023-02-05 15:07:10 +0100

    --amend

--- node/node.cpp
+++ node/node.cpp
@@ -1,5 +1,7 @@
 //create a node class who contain an operator and two pointer to node
 #include "node.h"
+#include <iostream>
+#include <stack>
 
 //constructor of Node
 Node::Node()
@@ -37,4 +39,37 @@ NodeVariable::NodeVariable(char  value)
 //getters
 Variable NodeVariable::getValue() { return value; }
 
+Node* Node::buildExpressionTree(const string& input) {
+    stack<Node*> nodes;
+    for (char c : input) {
+        if (isdigit(c)) {
+            nodes.push(new NodeConstante(c - '0'));
+        } else if (isalpha(c)) {
+            nodes.push(new NodeVariable(c));
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            Node* right = nodes.top();
+            nodes.pop();
+            Node* left = nodes.top();
+            nodes.pop();
+            nodes.push(new NodeOperator(Operator(c), left, right));
+        }
+    }
+    return nodes.top();
+}
+
+void Node::printExpressionTree(Node* node) {
+    if (!node) return;
+
+    if (NodeConstante* nc = dynamic_cast<NodeConstante*>(node)) {
+        cout << nc->getValue().getValue() << " ";
+    } else if (NodeVariable* nv = dynamic_cast<NodeVariable*>(node)) {
+        cout << nv->getValue().getIdent() << " ";
+    } else if (NodeOperator* no = dynamic_cast<NodeOperator*>(node)) {
+        cout << "(";
+        printExpressionTree(no->getLeft());
+        cout << no->getOp().getOp() << " ";
+        printExpressionTree(no->getRight());
+        cout << ")";
+    }
+}
 
